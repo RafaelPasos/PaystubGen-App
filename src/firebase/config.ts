1d19a8d3b9026064d23282dcd8c302b736f5dc45
rafael pasos <alirafael.pasos@gmail.com>
2025-10-19 18:09:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6461357104-730f1",
+  "appId": "1:41314376425:web:eacb309c1b11202373ee78",
+  "apiKey": "AIzaSyAdMDfkyur9QQP983YHUh1Y6e9CFvNDMGo",
+  "authDomain": "studio-6461357104-730f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "41314376425"
+};
